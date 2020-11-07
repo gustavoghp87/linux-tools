21636af14c6e011a5ac8a6079b0f64b255debdc3
gustavoghp87 <ghp.2120@gmail.com>
2020-11-07 11:35:27 -0300

    monitor $interface

--- monitor.sh
+++ monitor.sh
@@ -2,19 +2,22 @@
 
 home=$(pwd)
 
+for INTERFACE in "$@"
+do
 echo
 echo "\e[0;33m
-    *** wlan0 TO MONITOR MODE *** "
+    *** $INTERFACE TO MONITOR MODE *** "
 echo
 sleep 2
 
 airmon-ng &&
 airmon-ng check kill &&
-ip link set wlan0 down &&
-iw dev wlan0 set type monitor &&
-ip link set wlan0 up &&
-iw wlan0 set txpower 3000 &&
-iw wlan0 info
+ip link set $INTERFACE down &&
+airmon-ng start $INTERFACE &&
+# iw dev $INTERFACE set type monitor &&
+ip link set $INTERFACE up &&
+# iw $INTERFACE set txpower 3000 &&
+iw $INTERFACE info
 done
 
 echo "\n\e[0;31m *** OK! ***"
