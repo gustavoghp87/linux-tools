964d2fa703c32dad8f5305b4ac266007a0c0dd51
gustavoghp87 <ghp.2120@gmail.com>
2021-01-07 20:41:35 -0300

    amnesic kali

--- barrani.sh
+++ barrani.sh
@@ -4,12 +4,12 @@ home=$(pwd)
 
 for INTERFACE in "$@"
 do
-echo
-printf "\e[0;33m
-    *** BARRANING $INTERFACE *** "
-echo
-sleep 2
-ifconfig $INTERFACE down && macchanger -r $INTERFACE && ifconfig $INTERFACE up
+    echo
+    printf "\e[0;33m
+        *** BARRANING $INTERFACE *** "
+    echo
+    sleep 2
+    ifconfig $INTERFACE down && macchanger -r $INTERFACE && ifconfig $INTERFACE up
 done
 
 printf "\n\e[0;31m *** OK! ***"
--- wifite-full.sh
+++ wifite-full.sh
@@ -1,5 +1,22 @@
+#!bin/bash
 
-git clone https://github.com/devr82/wifite2;
-apt-get install hcxdumptool --assume-yes;
-apt install hcxtools;
-git clone https://github.com/JPaulMora/Pyrit;
+# git clone https://github.com/devr82/wifite2
+# &&
+# git clone https://github.com/JPaulMora/Pyrit
+# &&
+
+apt update
+&&
+apt-get update
+&&
+apt-get install hcxdumptool --assume-yes
+&&
+apt install hcxtools --assume-yes
+&&
+apt-get install python2.7-dev libssl-dev zlib1g-dev libpcap-dev
+&&
+python2 /media/kali/persistence/Pyrit/setup.py clean
+&&
+python2 /media/kali/persistence/Pyrit/setup.py build
+&&
+python2 /media/kali/persistence/Pyrit/setup.py install
