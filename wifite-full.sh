3aca993548765d3228a3667c247a299237472c00
root <ghp.2120@gmail.com>
2021-01-07 23:59:16 +0000

    amnesic kali 2

--- wifite-full.sh
+++ wifite-full.sh
@@ -1,22 +1,13 @@
 #!bin/bash
 
-# git clone https://github.com/devr82/wifite2
-# &&
-# git clone https://github.com/JPaulMora/Pyrit
-# &&
+# git clone https://github.com/devr82/wifite2;
+# git clone https://github.com/JPaulMora/Pyrit;
 
-apt update
-&&
-apt-get update
-&&
-apt-get install hcxdumptool --assume-yes
-&&
-apt install hcxtools --assume-yes
-&&
-apt-get install python2.7-dev libssl-dev zlib1g-dev libpcap-dev
-&&
-python2 /media/kali/persistence/Pyrit/setup.py clean
-&&
-python2 /media/kali/persistence/Pyrit/setup.py build
-&&
-python2 /media/kali/persistence/Pyrit/setup.py install
+apt update;
+apt-get update;
+apt-get install hcxdumptool --assume-yes;
+apt install hcxtools --assume-yes;
+apt-get install python2.7-dev libssl-dev zlib1g-dev libpcap-dev --assume-yes;
+python2 /media/kali/persistence/Pyrit/setup.py clean;
+python2 /media/kali/persistence/Pyrit/setup.py build;
+python2 /media/kali/persistence/Pyrit/setup.py install;
