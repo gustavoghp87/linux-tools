2a933b6b8685ea9913530611fc78b497de45a0fc
gustavoghp87 <ghp.2120@gmail.com>
2021-01-08 13:25:54 -0300

    wifite, unmonitor

--- unmonitor.sh
+++ unmonitor.sh
@@ -4,18 +4,22 @@ home=$(pwd)
 
 for INTERFACE in "$@"
 do
-echo
-printf "\e[0;33m
-    *** $INTERFACE TO MANAGED MODE *** "
-echo
-sleep 2
+	echo
+	printf "\e[0;33m
+	    *** $INTERFACE TO MANAGED MODE *** "
+	echo
+	sleep 2
 
-airmon-ng &&
-airmon-ng check kill &&
-ip link set $INTERFACE down &&
-airmon-ng stop $INTERFACE &&
-ip link set $INTERFACE up &&
-NetworkManager
+	airmon-ng &&
+	airmon-ng check kill &&
+	ip link set $INTERFACE down &&
+	echo "finished ip link set ... down" &&
+	airmon-ng stop $INTERFACE &&
+	echo "finished airmon-ng stop" &&
+	ip link set $INTERFACE up &&
+	echo "ip link set ... up" &&
+	NetworkManager &&
+	echo "finished NetworkManager"
 done
 
 printf "\n\e[0;31m *** OK! ***"
--- wifite-full.sh
+++ wifite-full.sh
@@ -4,10 +4,29 @@
 # git clone https://github.com/JPaulMora/Pyrit;
 
 apt update;
+echo "\n-------- ready --------\n";
+
 apt-get update;
+echo "\n-------- ready --------\n";
+
 apt-get install hcxdumptool --assume-yes;
+echo "\n-------- ready --------\n";
+
 apt install hcxtools --assume-yes;
+echo "\n-------- ready --------\n";
+
 apt-get install python2.7-dev libssl-dev zlib1g-dev libpcap-dev --assume-yes;
+echo "\n-------- ready --------\n";
+
+cd /media/kali/persistence/Pyrit/cpyrit;
 python2 /media/kali/persistence/Pyrit/setup.py clean;
+echo "\n-------- ready --------\n";
+
 python2 /media/kali/persistence/Pyrit/setup.py build;
+echo "\n-------- ready --------\n";
+
 python2 /media/kali/persistence/Pyrit/setup.py install;
+echo "\n-------- ready --------\n";
+
+python /media/kali/persistence/wifite2/Wifite.py
+echo "\n-------- ready --------\n";
