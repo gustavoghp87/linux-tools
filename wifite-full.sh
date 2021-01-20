c0f7b162e44352bd9d8a255b54e4dbf01c24cc1b
Kali Live user <ghp.2120@gmail.com>
2021-01-20 15:36:26 +0000

    wifite persistence

--- wifite-full.sh
+++ wifite-full.sh
@@ -1,32 +1,42 @@
 #!bin/bash
 
+echo "--------> Run from /home and sudo"
 # git clone https://github.com/devr82/wifite2;
 # git clone https://github.com/JPaulMora/Pyrit;
 
 apt update;
-echo "\n-------- ready --------\n";
+echo
+echo "-------- ready --------";
+echo
 
 apt-get update;
-echo "\n-------- ready --------\n";
+echo
+echo "\n-------- ready --------";
+echo
 
 apt-get install hcxdumptool --assume-yes;
-echo "\n-------- ready --------\n";
+echo
+echo "-------- ready --------";
+echo
 
 apt install hcxtools --assume-yes;
-echo "\n-------- ready --------\n";
+echo
+echo "-------- ready --------";
+echo
 
 apt-get install python2.7-dev libssl-dev zlib1g-dev libpcap-dev --assume-yes;
+echo
+echo "-------- ready --------";
+echo
+
+python2 Pyrit/setup.py clean;
 echo "\n-------- ready --------\n";
 
-cd /media/kali/persistence/Pyrit/cpyrit;
-python2 /media/kali/persistence/Pyrit/setup.py clean;
+python2 Pyrit/setup.py build;
 echo "\n-------- ready --------\n";
 
-python2 /media/kali/persistence/Pyrit/setup.py build;
+python2 Pyrit/setup.py install;
 echo "\n-------- ready --------\n";
 
-python2 /media/kali/persistence/Pyrit/setup.py install;
-echo "\n-------- ready --------\n";
-
-python /media/kali/persistence/wifite2/Wifite.py
+python wifite2/Wifite.py
 echo "\n-------- ready --------\n";
